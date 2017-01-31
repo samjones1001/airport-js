830f8e1c4943f416fa51377c29f1674c586503e2
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-31 12:44:24 +0000

    Up to user story 3

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,6 +6,10 @@
 // So I can get passengers on the way to their destination 
 // I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
+//As an air traffic controller 
+//To ensure safety 
+//I want to prevent takeoff when weather is stormy 
+
 describe('Airport', function() {
 
 	var airport;
@@ -22,7 +26,33 @@ describe('Airport', function() {
 
 		it('and stores it', function() {
 			(airport.receive(plane))
-			expect(airport._planes).toContain(plane)	
+			expect(airport.planeReturn()).toContain(plane)	
+		});
+	});
+
+	describe('take off a landed plane when weather is sunny', function() {
+		it('and confirms it has taken off', function() {
+			airport.receive(plane);
+			expect(airport.takeOff(plane)).toEqual("the plane has taken off");
+		});
+
+		it('and stores it', function() {
+			airport.receive(plane);
+			airport.takeOff(plane);
+			expect(airport.planeReturn()).not.toContain(plane);
+		});
+	});
+
+	describe('cannot take off a landed plane weather is stormy', function() {
+		it('raise an error', function() {
+			airport.receive(plane);
+			//expect(airport.takeOff(plane)).toEqual("the plane has taken off");
+		});
+
+		it('plane is still in array', function() {
+			airport.receive(plane);
+			airport.takeOff(plane);
+			//expect(airport.planeReturn()).not.toContain(plane);
 		});
 	});
 
--- src/Airport.js
+++ src/Airport.js
@@ -7,3 +7,13 @@ Airport.prototype.receive = function(plane) {
 	console.log(this._planes);
 	return "the plane has landed";
 };
+
+Airport.prototype.planeReturn = function() {
+	return this._planes;
+}
+
+Airport.prototype.takeOff = function(plane){
+	var position = this._planes.indexOf(plane); 
+	this._planes.splice(position,1);
+	return "the plane has taken off"
+}
